f12d33036b134c1c792ce764535a37f0a4c946ba
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-14 10:12:18 +0200

    Add another dependency Laptop and use autowire annotation to inject it since Programmer does not have direct access to application context

---
+++ src/main/java/inc/sims/hustles/SpringBootDemo/Laptop.java
@@ -0,0 +1,11 @@
+package inc.sims.hustles.SpringBootDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compileCode(){
+        System.out.println("Compiling your code...");
+    }
+}
--- src/main/java/inc/sims/hustles/SpringBootDemo/Programmer.java
+++ src/main/java/inc/sims/hustles/SpringBootDemo/Programmer.java
@@ -1,11 +1,16 @@
 package inc.sims.hustles.SpringBootDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Programmer {
 
+    @Autowired
+    Laptop laptop;
+
     public void code(){
         System.out.println("Writing code!");
+        laptop.compileCode();
     }
 }
