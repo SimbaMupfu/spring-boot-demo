c190bcd6be822127dadc9e169161baea5e4455f6
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-14 09:52:54 +0200

    Initial commit

---
+++ src/test/java/inc/sims/hustles/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.sims.hustles.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
