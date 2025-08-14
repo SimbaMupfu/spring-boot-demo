c0bdbfa236bf94fc461ac1fcda7018af45f11f1c
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-14 10:00:18 +0200

    Create an object called Programmer and inject it into application context.

---
+++ src/main/java/inc/sims/hustles/SpringBootDemo/Programmer.java
@@ -0,0 +1,11 @@
+package inc.sims.hustles.SpringBootDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Programmer {
+
+    public void code(){
+        System.out.println("Writing code!");
+    }
+}
