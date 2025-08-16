147c243cf57723767b50ffb7ee46936eeda069f6
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-16 14:22:15 +0200

    Refactor Programmer and Laptop to make them models.

--- src/main/java/inc/sims/hustles/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/inc/sims/hustles/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,5 +1,6 @@
 package inc.sims.hustles.SpringBootDemo;
 
+import inc.sims.hustles.SpringBootDemo.models.Programmer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
