501a3a3d8d138ecac2d94d330be460d0477f5ba1
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-16 15:16:55 +0200

    Call the services.

--- src/main/java/inc/sims/hustles/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/inc/sims/hustles/SpringBootDemo/SpringBootDemoApplication.java
@@ -3,6 +3,7 @@ package inc.sims.hustles.SpringBootDemo;
 import inc.sims.hustles.SpringBootDemo.models.Laptop;
 import inc.sims.hustles.SpringBootDemo.models.Programmer;
 import inc.sims.hustles.SpringBootDemo.service.LaptopService;
+import inc.sims.hustles.SpringBootDemo.service.ProgrammerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,9 +15,14 @@ public class SpringBootDemoApplication {
 		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
 
 		Laptop laptop = context.getBean(Laptop.class);
-
 		LaptopService laptopService = context.getBean(LaptopService.class);
 		laptopService.addLaptop(laptop);
+		laptopService.removeLaptop(laptop);
+
+		Programmer programmer = context.getBean(Programmer.class);
+		ProgrammerService programmerService = context.getBean(ProgrammerService.class);
+		programmerService.addProgrammer(programmer);
+		programmerService.removeProgrammer(programmer);
 	}
 
 }
